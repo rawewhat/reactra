30a6f773cb0d514243f7b8463e98654d1edd66f9
Cheng Sokdara <chengsokdara@gmail.com>
2019-11-12 13:07:07 +0700

    Integrated Ant Design with less style override support.

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from 'antd'
 
 const HomeScreen = () => {
   return (
@@ -6,6 +7,7 @@ const HomeScreen = () => {
       {typeof window !== 'undefined' && window.document !== undefined
         ? 'CLIENT'
         : 'SERVER'}
+        <Button>AntD</Button>
     </div>
   );
 };
